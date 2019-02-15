543275c085e063adda0d482bc9cfd0c22e70f4c7
RaphaelTheFool <19533739+RaphaelTFool@users.noreply.github.com>
2019-02-15 15:45:22 +0800

    update tcpdemo with a simple heartbeat exchange
    simply enhance recv and send, and improve with a heartbeat exchange

--- tcp_demo/tcp_client.c
+++ tcp_demo/tcp_client.c
@@ -10,6 +10,7 @@
 #include<string.h>
 #include<errno.h>
 #include<unistd.h>
+#include<time.h>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<sys/select.h>
@@ -55,6 +56,90 @@ int tcp_connect(const char *ip, unsigned short port)
     return sockfd;
 }
 
+int tcp_send(int sockfd, char *buff, int bufflen)
+{
+    char send_buff[1024];
+    int send_ttlen = 0;
+    int send_len = 0;
+
+    memset(send_buff, 0x0, sizeof(send_buff));
+    snprintf(send_buff, sizeof(send_buff), "%05d%s", bufflen, buff);
+    send_ttlen = strlen(send_buff);
+    ERR_PRINT("send_bufflen: %d, send_buff: %s", send_ttlen, send_buff);
+
+    char *sendp = send_buff;
+    int already_send = 0;
+    while (send_ttlen > 0)
+    {
+        send_len = send(sockfd, sendp, send_ttlen, 0);
+        switch (send_len)
+        {
+            case -1:
+            if (errno == EINTR)
+                continue;
+            else
+            {
+                ERR_PRINT("send msg error");
+                return -1;
+            }
+            break;
+
+            case 0:
+            ERR_PRINT("send error");
+            return -1;
+            break;
+
+            default:
+            already_send += send_len;
+            sendp += send_len;
+            send_ttlen -= send_len;
+            break;
+        }
+    }
+
+    ERR_PRINT("send len: %d", already_send);
+    return 0;
+}
+
+int tcp_recv(int sockfd, char *buff, int *buff_len)
+{
+    char len_str[6];
+    int recv_len;
+    int msg_len;
+
+    if (NULL == buff || *buff_len <= 0)
+    {
+        ERR_PRINT("input para invalid");
+        return -1;
+    }
+
+    memset(len_str, 0x0, sizeof(len_str));
+    recv_len = recv(sockfd, len_str, sizeof(len_str) - 1, MSG_WAITALL);
+    if (5 != recv_len)
+    {
+        ERR_PRINT("@@ failed to get msg len, recv_len = %d", recv_len);
+        return -1;
+    }
+    else
+    {
+        ERR_PRINT("len_str: %s", len_str);
+        msg_len = atoi(len_str);
+        ERR_PRINT("practical msg length: %d", msg_len);
+        /* 如果消息格式不规范，这里会导致下一个消息的解析出错 */
+        *buff_len = (msg_len > 0 && msg_len < *buff_len - 1) ? msg_len: *buff_len - 1;
+        ERR_PRINT("buff_len = %d", *buff_len);
+        recv_len = recv(sockfd, buff, *buff_len, MSG_WAITALL);
+        if (recv_len <= 0)
+        {
+            ERR_PRINT("@@ failed to receive message");
+            return -1;
+        }
+        ERR_PRINT("buff: %s", buff);
+    }
+
+    return 0;
+}
+
 void usage(char *name)
 {
     printf("Usage: %s <-s ip address> <-p port>\n", name);
@@ -62,12 +147,14 @@ void usage(char *name)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, listenfd, connfd;
+    int sockfd, connfd;
     struct sockaddr_in client;
     socklen_t addrlen = sizeof(client);
     int opt;
+    int maxfd;
     char s_ip[INET_ADDRSTRLEN];
     unsigned short s_port;
+    int send_times = 0;
 
     int time = 3;
 
@@ -105,12 +192,6 @@ int main(int argc, char *argv[])
         }
     }
 
-    if (argc < 5 || argc > 7)
-    {
-        fprintf(stderr, "command line option and argument error\n\n");
-        usage(argv[0]);
-        exit(EXIT_FAILURE);
-    }
     printf("optind = %d, argc = %d\n", optind, argc);
     for (i = 0; i < optind; i++)
     {
@@ -120,23 +201,106 @@ int main(int argc, char *argv[])
     {
         printf("No.%d: %s\n", i, argv[i]);
     }
+    if (argc < 5 || argc > 7)
+    {
+        fprintf(stderr, "command line option and argument error\n\n");
+        usage(argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
     if (-1 == (sockfd = tcp_connect(s_ip, s_port)))
     {
         ERR_PRINT("connect server error");
         exit(EXIT_FAILURE);
     }
+    maxfd = sockfd + 1;
 
     while (1)
     {
         char buff[1024];
-        snprintf(buff, sizeof(buff), "hello server, my pid is %d\n", getpid());
-        int send_len = send(sockfd, buff, strlen(buff), 0);
-        printf("send_len: %d\n", send_len);
+        fd_set nreads;
+        struct timeval timeout;
+
+        memset(buff, 0x0, sizeof(buff));
+        snprintf(buff, sizeof(buff), "1:hello server, my pid is %d.", getpid());
+        /* 第一次send */
+        int send_len = tcp_send(sockfd, buff, strlen(buff));
+        if (0 == send_len)
+        {
+            send_times++;
+            if (send_times >= 3)
+            {
+                #if 0
+                /* 尝试sleep打断两次send */
+                usleep(5);
+                /* 依然会随机粘包 */
+                #endif
+                ERR_PRINT("### send tcp heartbeat probe ###");
+                buff[0] = '0';
+                buff[1] = '\0';
+                /* 第二次send */
+                /* 连续的两次send导致的tcp流上的粘包，从而让交互逻辑出现了偏差 */
+                send_len = tcp_send(sockfd, buff, 1);
+                if (send_len == 0)
+                {
+                    ERR_PRINT("heartbeat send");
+                    timeout.tv_sec = 10;
+                    timeout.tv_usec = 0;
+
+                    FD_ZERO(&nreads);
+                    FD_SET(sockfd, &nreads);
+                    maxfd = sockfd + 1;
+
+                    int sret = select(maxfd, &nreads, NULL, NULL, &timeout);
+                    switch (sret)
+                    {
+                        case -1:
+                        ERR_PRINT("select error: %s, exit ...", strerror(errno));
+                        close(sockfd);
+                        exit(EXIT_FAILURE);
+                        break;
+
+                        case 0:
+                        ERR_PRINT("select timeout, exit ...");
+                        close(sockfd);
+                        exit(EXIT_FAILURE);
+                        break;
+
+                        default:
+                        if (FD_ISSET(sockfd, &nreads))
+                        {
+                            memset(buff, 0x0, sizeof(buff));
+                            int buff_tlen = sizeof(buff);
+                            int recv_len = tcp_recv(sockfd, buff, &buff_tlen);
+                            if (recv_len < 0)
+                            {
+                                ERR_PRINT("tcp_recv error: %s", strerror(errno));
+                                close(sockfd);
+                                exit(EXIT_FAILURE);
+                            }
+                            else
+                            {
+                                if (buff[0] == '$')
+                                {
+                                    ERR_PRINT("### tcp connection OK ###");
+                                    send_times = 0;
+                                }
+                                else
+                                {
+                                    ERR_PRINT("unknown error");
+                                    close(sockfd);
+                                    exit(EXIT_FAILURE);
+                                }
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
         sleep(time);
     }
 
-    close(listenfd);
-
+    close(sockfd);
     return 0;
 }
--- tcp_demo/tcp_server.c
+++ tcp_demo/tcp_server.c
@@ -13,13 +13,40 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<sys/select.h>
+#include<sys/wait.h>
 #include<arpa/inet.h>
 #include<netinet/in.h>
 #include<time.h>
 #include<signal.h>
 
+#define DEFAULT_TIME_OUT 1800
 #define ERR_PRINT(fmt, ...) \
         printf("[%-6.5s:%6.5d]"fmt"\n", __func__, __LINE__, ##__VA_ARGS__)
+
+typedef void (*sigfunc_t)(int);
+
+sigfunc_t register_signal(
+    int signo,
+    int flags,
+    sigfunc_t sig_handler
+    )
+{
+    struct sigaction act, oact;
+
+    sigemptyset(&act.sa_mask);
+    sigaddset(&act.sa_mask, SIGINT);
+    act.sa_flags = flags;
+    act.sa_handler = sig_handler;
+
+    if (-1 == sigaction(signo, &act, &oact))
+    {
+        ERR_PRINT("register signal %d error: %s",
+                 signo, strerror(errno));
+        return SIG_ERR;
+    }
+
+    return oact.sa_handler;
+}
 
 int tcp_server(const char *ip, unsigned short port)
 {
@@ -84,6 +111,89 @@ int tcp_accept(int listenfd, struct sockaddr *addr, socklen_t *addrlen)
     return connfd;
 }
 
+int tcp_send(int sockfd, char *buff, int bufflen)
+{
+    char send_buff[1024];
+    int send_len = 0;
+    int send_ttlen = 0;
+
+    snprintf(send_buff, sizeof(send_buff), "%05d%s", bufflen, buff);
+    send_ttlen = strlen(send_buff);
+    ERR_PRINT("send_bufflen: %d, send_buff: %s", send_ttlen, send_buff);
+
+    char *sendp = send_buff;
+    int already_send = 0;
+    while (send_ttlen > 0)
+    {
+        send_len = send(sockfd, sendp, send_ttlen, 0);
+        switch (send_len)
+        {
+            case -1:
+            if (errno == EINTR)
+                continue;
+            else
+            {
+                ERR_PRINT("send msg error");
+                return -1;
+            }
+            break;
+
+            case 0:
+            ERR_PRINT("send error");
+            return -1;
+            break;
+
+            default:
+            already_send += send_len;
+            sendp += send_len;
+            send_ttlen -= send_len;
+            break;
+        }
+    }
+
+    ERR_PRINT("send len: %d", already_send);
+    return 0;
+}
+
+int tcp_recv(int sockfd, char *buff, int *buff_len)
+{
+    char len_str[6];
+    int recv_len;
+    int msg_len;
+
+    if (NULL == buff || *buff_len <= 0)
+    {
+        ERR_PRINT("input para invalid");
+        return -1;
+    }
+
+    memset(len_str, 0x0, sizeof(len_str));
+    recv_len = recv(sockfd, len_str, sizeof(len_str) - 1, MSG_WAITALL);
+    if (5 != recv_len)
+    {
+        ERR_PRINT("@@ failed to get msg len, recv_len = %d", recv_len);
+        return -1;
+    }
+    else
+    {
+        ERR_PRINT("len_str: %s", len_str);
+        msg_len = atoi(len_str);
+        ERR_PRINT("practical msg length: %d", msg_len);
+        /* 如果消息格式不规范，这里会导致下一个消息的解析出错 */
+        *buff_len = (msg_len > 0 && msg_len < *buff_len - 1) ? msg_len: *buff_len - 1;
+        ERR_PRINT("buff_len = %d", *buff_len);
+        recv_len = recv(sockfd, buff, *buff_len, MSG_WAITALL);
+        if (recv_len != *buff_len)
+        {
+            ERR_PRINT("@@ failed to receive message");
+            return -1;
+        }
+        ERR_PRINT("buff: %s", buff);
+    }
+
+    return 0;
+}
+
 int children_write_msg(char *filename, char *msg, int msg_len)
 {
     FILE *fp;
@@ -116,7 +226,7 @@ int children_write_msg(char *filename, char *msg, int msg_len)
         return -1;
     }
 
-    ERR_PRINT("write_len = %d", write_len);
+    ERR_PRINT("childpid: %d: write_len = %d", getpid(),  write_len);
 
     fclose(fp);
 
@@ -134,13 +244,14 @@ int main(int argc, char *argv[])
     struct sockaddr_in client;
     socklen_t addrlen = sizeof(client);
     int opt;
+    int timeout = DEFAULT_TIME_OUT;
     char s_ip[INET_ADDRSTRLEN];
     unsigned short s_port;
     pid_t child;
 
     int i = 0;
 
-    while (-1 != (opt = getopt(argc, argv, "hp:s:")))
+    while (-1 != (opt = getopt(argc, argv, "hp:s:t:")))
     {
         switch (opt)
         {
@@ -156,6 +267,12 @@ int main(int argc, char *argv[])
             case 's':
             memset(s_ip, 0x0, sizeof(s_ip));
             strncpy(s_ip, optarg, sizeof(s_ip));
+            break;
+
+            case 't':
+            timeout = atoi(optarg);
+            if (timeout <= 0)
+                timeout = DEFAULT_TIME_OUT;
             break;
 
             default:
@@ -177,10 +294,16 @@ int main(int argc, char *argv[])
     {
         printf("No.%d: %s\n", i, argv[i]);
     }
-    if (argc != 5)
+    if (argc < 5 || argc > 7)
     {
         fprintf(stderr, "command line option and argument error\n\n");
         usage(argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (SIG_ERR == register_signal(SIGCHLD, SA_NOCLDWAIT, SIG_IGN))
+    {
+        ERR_PRINT("register sig_child error");
         exit(EXIT_FAILURE);
     }
 
@@ -206,6 +329,13 @@ int main(int argc, char *argv[])
         }
         else
         {
+            char cli_ip[INET_ADDRSTRLEN];
+            unsigned short cli_port;
+            memset(cli_ip, 0x0, sizeof(cli_ip));
+            ERR_PRINT("client info: %s:%u",
+                     inet_ntop(AF_INET, &client.sin_addr, cli_ip, sizeof(cli_ip)),
+                     ntohs(client.sin_port));
+
             child = fork();
             switch (child)
             {
@@ -219,29 +349,99 @@ int main(int argc, char *argv[])
                 char msg_buff[1024];
                 char filename[1024];
                 int recv_len;
+                fd_set nreads;
+                struct timeval ageing_time;
+                int maxnfd = connfd + 1;
+                int sret = 0;
 
                 snprintf(filename, sizeof(filename), "child.%d", getpid());
 
                 while (1)
                 {
-                    memset(buff, 0x0, sizeof(buff));
-                    recv_len = recv(connfd, buff, sizeof(buff), 0);
-                    if (recv_len > 0)
+                    FD_ZERO(&nreads);
+                    FD_SET(connfd, &nreads);
+                    ageing_time.tv_sec = timeout;
+                    ageing_time.tv_usec = 0;
+
+                    sret = select(maxnfd, &nreads, NULL, NULL, &ageing_time);
+                    switch (sret)
                     {
-                        snprintf(msg_buff, sizeof(msg_buff), "mypid: %d, recv len: %d, recv msg: %s\n",
-                              getpid(), recv_len, buff);
-                        children_write_msg(filename, msg_buff, strlen(msg_buff));
-                    }
-                    else if (recv_len == 0)
-                    {
+                        case -1:
+                        if (EINTR == errno)
+                            continue;
+                        else
+                        {
+                            ERR_PRINT("select error: %s", strerror(errno));
+                            goto OUT_LOOP;
+                        }
+                        break;
+
+                        case 0:
+                        ERR_PRINT("in time %d seconds, no data arrives, shutdown this connection", ageing_time.tv_sec);
+                        #if 0
+                        if (-1 == shutdown(connfd, SHUT_RDWR))
+                        {
+                            ERR_PRINT("failed to shutdown connection, exit ...");
+                            goto OUT_LOOP;
+                        }
+                        #else
+                        close(connfd);
+                        exit(EXIT_FAILURE);
+                        #endif
+                        break;
+
+                        default:
+                        if (FD_ISSET(connfd, &nreads))
+                        {
+                            //ERR_PRINT("child[%d]::after time %d sec %d usec",
+                            //         getpid(), ageing_time.tv_sec, ageing_time.tv_usec);
+                            int buff_tlen = sizeof(buff);
+                            memset(buff, 0x0, sizeof(buff));
+                            recv_len = tcp_recv(connfd, buff, &buff_tlen);
+                            if (recv_len == 0)
+                            {
+                                ERR_PRINT("buff[0] = %c", buff[0]);
+                                switch (buff[0])
+                                {
+                                    case '1':
+                                    printf("buff: %s", buff);
+                                    snprintf(msg_buff, sizeof(msg_buff), "mypid: %d, msg len: %d, recv msg: %s\n",
+                                          getpid(), strlen(buff), buff);
+                                    children_write_msg(filename, msg_buff, strlen(msg_buff));
+                                    break;
+
+                                    case '0':
+                                    ERR_PRINT("### receive a tcp heartbeat ###");
+                                    memset(buff, 0x0, sizeof(buff));
+                                    buff[0] = '$';
+                                    int send_len = tcp_send(connfd, buff, 1);
+                                    if (send_len < 0)
+                                    {
+                                        ERR_PRINT("send heartbeat ack error: %s", strerror(errno));
+                                    }
+                                    break;
+
+                                    default:
+                                    ERR_PRINT("unknown message: %s", buff);
+                                    break;
+                                }
+                            }
+                            else if (recv_len < 0)
+                            {
+                                ERR_PRINT("some errors happens or client exits ...");
+                                goto OUT_LOOP;
+                            }
+                        }
                         break;
                     }
                 }
+OUT_LOOP:
                 close(connfd);
                 exit(EXIT_FAILURE);
                 break;
 
                 default:
+                close(connfd);
                 printf("father pid: %d, wait again\n", getpid());
                 break;
             }
